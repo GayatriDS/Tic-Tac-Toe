5407f6ef824807e1f59bde1371851fefa474c956
GayatriDS <34643526+GayatriDS@users.noreply.github.com>
2020-07-16 13:26:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Square from "./component/Square";
+import Game from "./component/Game";
+
+function App() {
+  return (
+    <div className="App">
+      <Game></Game>
+    </div>
+  );
+}
+
+export default App;
